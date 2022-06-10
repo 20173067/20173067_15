9ba2ecd31d3b06b99cffd3a6f557ee761c0de3bf
PC USER <yenini0709@gmail.com>
2022-06-10 10:37:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20173067_15_1/MovieList.java
@@ -0,0 +1,5 @@
+package com.example.a20173067_15_1;
+
+public class MovieList {
+    MovieListResult boxOfficeResult;
+}
---
+++ app/src/main/java/com/example/a20173067_15_1/MovieListResult.java
@@ -0,0 +1,10 @@
+package com.example.a20173067_15_1;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
